9a22884fbb4e86248c9985fdc88407a1c38def45
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-13 03:59:07 +0900

    feat: 회원 가입 페이지 생성

--- client/pages/SignUp/index.tsx
+++ client/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
-import useInput from '@hooks/useInput';
 import React, { useCallback, useState } from 'react';
+import useInput from '@hooks/useInput';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { Success, Form, Error, Label, Input, LinkContainer, Button, Header } from './styles';
 
@@ -10,7 +11,12 @@ const SignUp = () => {
   const [password, , setPassword] = useInput('');
   const [passwordCheck, , setPasswordCheck] = useInput('');
   const [mismatchError, setMismatchError] = useState(false);
+  const [signUpError, setSignUpError] = useState('');
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
 
+  /**
+   * 바밀번호 입력에 대한 Handler로 state 저장 및 비밀번호 확인 대조 체크를 한다.
+   */
   const onChangePassword = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
       setPassword(event.target.value);
@@ -19,6 +25,9 @@ const SignUp = () => {
     [passwordCheck],
   );
 
+  /**
+   * 입력한 비밀번호에 대한 Handler로 state 저장 및 입력한 비밀번호와 대조 한다.
+   */
   const onChangePasswordCheck = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
       setPasswordCheck(event.target.value);
@@ -27,15 +36,33 @@ const SignUp = () => {
     [password],
   );
 
+  /**
+   * 입력 완료시 서버로 보내는 submit handler로 닉네임과 비밀번호 확인이 되지 않으면 에러를 발생한다.
+   * 정상 확인이 될 경우 서버애 post 진행한다.
+   */
   const onSubmit = useCallback(
     (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
-      console.log(email, nickname, password, passwordCheck);
-      if (!mismatchError && !nickname) {
-        console.log('서버로 회원가입 하기');
+      if (!mismatchError && nickname) {
+        setSignUpError('');
+        setSignUpSuccess(false);
+        axios
+          .post('/api/users', {
+            email,
+            nickname,
+            password,
+          })
+          .then((response) => {
+            console.log(response);
+            setSignUpSuccess(true);
+          })
+          .catch((error) => {
+            setSignUpError(error.response.data);
+          })
+          .finally(() => {});
       }
     },
-    [email, nickname, password, passwordCheck],
+    [email, nickname, password, passwordCheck, mismatchError],
   );
 
   return (
@@ -73,8 +100,8 @@ const SignUp = () => {
           </div>
           {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
           {!nickname && <Error>닉네임을 입력해주세요.</Error>}
-          {/* {signUpError && <Error>{signUpError}</Error>}
-          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
+          {signUpError && <Error>{signUpError}</Error>}
+          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
         </Label>
         <Button type="submit">회원가입</Button>
       </Form>
