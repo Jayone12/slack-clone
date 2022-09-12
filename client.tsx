6630a5b55ef4e8e5462b43815204a93418f09c81
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-13 01:05:01 +0900

    폴더 구조와 리액트 라우터 설정

--- client.tsx
+++ client.tsx
@@ -1,8 +1,12 @@
-import { render } from 'react-dom';
 import React from 'react';
-import App from './layouts/App';
+import App from '@layouts/App';
 
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 const container = document.getElementById('app');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
