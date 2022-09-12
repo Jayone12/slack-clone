a6e398008340974d151ee839b2db94c1de8ea064
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-13 03:12:36 +0900

    주석 추가

--- client/hooks/useInput.ts
+++ client/hooks/useInput.ts
@@ -1,11 +1,15 @@
 import React, { ChangeEvent, Dispatch, SetStateAction, UIEvent, useCallback, useState } from 'react';
 
-type ReturnTypes<T = any> = [T, (event: ChangeEvent<HTMLInputElement>) => void, Dispatch<SetStateAction<T>>];
+type ReturnTypes<T> = [T, (event: ChangeEvent<HTMLInputElement>) => void, Dispatch<SetStateAction<T>>];
 
-const useInput = <T = any>(initialData: T): ReturnTypes => {
+/**
+ * 커스텀 hook으로 텍스트 입력값을 받아 처리하는 hook이다.
+ * @param 초기값
+ */
+const useInput = (initialData: string): ReturnTypes<string> => {
   const [value, setValue] = useState(initialData);
   const handler = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value as unknown as T);
+    setValue(event.target.value);
   }, []);
   return [value, handler, setValue];
 };
